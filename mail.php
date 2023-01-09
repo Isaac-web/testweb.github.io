fe1826fb0d1af2ebfe63f014e01dc72dc6c0391c
Michael Opoku <michaelopoku790@gmail.com>
2023-01-09 07:24:49 +0000

    changes

--- mail.php
+++ mail.php
@@ -1,14 +1,13 @@
 <?php 
 
-/* $name = $_POST['name'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="Contact form\n\nFrom: $name \nMessage: $message";
 $recipient = "michaelopoku790@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
- */
-mail('michaelopoku790', 'hello', 'sdsds', 'From: michaelopoku790@gmail.com \r\n') or die("Error!");
-echo "Thank You! E-mail send.";
+
+
 
 ?>
